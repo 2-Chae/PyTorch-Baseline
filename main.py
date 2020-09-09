ae00e85e415dcb9447ef6c0f1f418032d0b0fd56
Chaehyeon Lee <123456ccdd@knu.ac.kr>
2020-09-10 07:02:16 +0900

    add scheduler

--- main.py
+++ main.py
@@ -19,11 +19,11 @@ from utils.misc import _create_info_folder, progress_bar
 parser = argparse.ArgumentParser(description='PyTorch Training Baseline')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--weight-decay', type=float, default=5e-4, help='weight decay')
-parser.add_argument('--epochs', type=int, default=30, help='# of epochs to train')
+parser.add_argument('--epochs', type=int, default=164, help='# of epochs to train')
 parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
 parser.add_argument('--save-image', action='store_true', default=False, help='save input image')
-parser.add_argument('--batch-train', type=int, default=256, help='batch size for train set')
-parser.add_argument('--batch-test', type=int, default=256, help='batch size for test set')
+parser.add_argument('--batch-train', type=int, default=128, help='batch size for train set')
+parser.add_argument('--batch-test', type=int, default=128, help='batch size for test set')
 args = parser.parse_args()
 
 print(args)
@@ -46,14 +46,14 @@ transform_test = transforms.Compose([
 
 # define dataset
 trainset = torchvision.datasets.CIFAR10(root='~/Downloads', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_train, shuffle=True, num_workers=2)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_train, shuffle=True, num_workers=4)
 
 testset = torchvision.datasets.CIFAR10(root='~/Downloads', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch_test, shuffle=False, num_workers=2)
+testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch_test, shuffle=False, num_workers=4)
 
 
 # Model
-net = models.resnet18()
+net = models.resnet18(pretrained=True)
 net = net.to(device)
 
 if device == 'cuda':
@@ -63,6 +63,7 @@ if device == 'cuda':
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[82, 123], gamma=0.1)
 
 # log
 board_writer = SummaryWriter()
@@ -131,7 +132,8 @@ for epoch in range(args.epochs):
         n_iter += 1
 
     test_acc = test(testloader)
-    # print('test_acc: %.3f' % test_acc)
+    print('test_acc: %.3f' % test_acc)
+    scheduler.step()
 
     row = {'epoch': str(epoch), 'train_acc': str(100.*correct/total), 'train_loss': str(train_loss/(batch_idx+1)), 'test_acc': str(test_acc)}
     csv_logger.writerow(row)
